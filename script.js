8db750ae270cabc4b96b3d2cd59e5cca10d1d761
Jens <jens.e.bouwens@gmail.com>
2020-10-07 08:25:23 +0200

    Last week of school work

--- script.js
+++ script.js
@@ -4,31 +4,37 @@ var citys = ["q=Viborg", "q=Manchester", "q=Newcastle,GB", "q=London,GB"];
 var otherstuff = "units=metric&lang=en";
 var urls = [https+""+citys[0]+"&"+otherstuff+"&"+apiKey, https+""+citys[1]+"&"+otherstuff+"&"+apiKey, https+""+citys[2]+"&"+otherstuff+"&"+apiKey, https+""+citys[3]+"&"+otherstuff+"&"+apiKey];
 
+var skills = [{ "name": "HTML", "proficiency": "67%", "color": "#d65709" }, { "name": "CSS", "proficiency": "45%", "color": "#099fd6" }, { "name": "PHP", "proficiency": "39%", "color": "#8b09d6" }, { "name": "JS", "proficiency": "44%", "color": "#d6bb09" }, { "name": "C#", "proficiency": "37%", "color": "#082dcf" }]
+
 window.onload = (event) => {
   console.log('page is fully loaded')
-
+  skills.forEach(element => {
+    createnewbar(element)
+  });
+/*
   // Viborg
   fetch(urls[0])
     .then(v=>v.json())
-      .then(v=>creatnewcard(v))
+      .then(v=>createnewcard(v))
 
   // Newcastle
   fetch(urls[2])
     .then(v=>v.json())
-      .then(v=>creatnewcard(v))
+      .then(v=>createnewcard(v))
 
   // Manchester
   fetch(urls[1])
     .then(v=>v.json())
-      .then(v=>creatnewcard(v))
+      .then(v=>createnewcard(v))
 
   // London
   fetch(urls[3])
     .then(v=>v.json())
-      .then(v=>creatnewcard(v))
+      .then(v=>createnewcard(v))
+*/
 }
 
-function creatnewcard(data) {
+function createnewcard(data) {
   var card = $(".card.template").clone().removeClass("template")
   card.find("img").attr("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`)
   card.find(".name b").text(data.name+", "+data.sys.country)
@@ -38,10 +44,19 @@ function creatnewcard(data) {
   card.find(".timezone").text("UTC "+data.timezone)
   card.find(".sunrise").text(timeWisserdry(data.sys.sunrise))
   card.find(".sunset").text(timeWisserdry(data.sys.sunset))
-  $(card).insertAfter(".template")
+  $(card).insertAfter(".card.template")
 }
 
-function timeWisserdry(unix){
+function createnewbar(element) {
+  var bar = $(".bar.template").clone().removeClass("template")
+  bar.find(".name").text(element.name)
+  bar.find(".proficiency").text(element.proficiency)
+  bar.find(".proficiency").css("width", element.proficiency)
+  bar.find(".proficiency").css("background-color", element.color)
+  $(bar).insertAfter(".bar.template")
+}
+
+function timeWisserdry(unix) {
   var date = new Date(unix * 1000)
   var hour = date.getHours()
   var min = "0" + date.getMinutes()
@@ -49,3 +64,13 @@ function timeWisserdry(unix){
   var riseFormat = hour + ':' + min.substr(-2) + ':' + sec.substr(-2)
   return riseFormat
 }
+
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    var str = $("#myInput").val()
+    var url = https+"q="+str+"&"+otherstuff+"&"+apiKey
+    fetch(url)
+      .then(v=>v.json())
+        .then(v=>createnewcard(v))
+  });
+});
